cf35a3edd8b59d494c409893357bff7d5c1bb353
AnUnsocialPigeon <jdturrell@Gmail.com>
2024-07-04 00:19:15 +0100

    Aestroid works

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -6,6 +6,7 @@ import '../App.css';
 import { Environment, OrbitControls, Sparkles } from '@react-three/drei';
 import SpaceStation from '../models/SpaceStation';
 import AudioPlayer from './AudioPlayer';
+import Aestroid from '../models/Aestroid';
 
 const Space = () => {
   return (
@@ -17,6 +18,8 @@ const Space = () => {
           <ambientLight intensity={1.5}/>
           <OrbitControls minDistance={0} maxDistance={15} panSpeed={0}/>
           <SpaceStation position={[0, 0, 0]} scale={[1, 1, 1]} />
+          <Aestroid size={1} position={[0, 0, 0]} scale={[1, 1, 1]}/>
+          <Aestroid position={[0, 1, 5]} scale={[0.004, 0.004, 0.004]}/>
           <Sparkles size={2} color={"#ffffff"} scale={[10,10,10]} />
         </Suspense>
         <Environment preset="sunset" />

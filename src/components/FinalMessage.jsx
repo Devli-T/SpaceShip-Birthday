ffa9cda79c9dfc34a7e219c21946ee3133a87aa5
AnUnsocialPigeon <jdturrell@Gmail.com>
2024-07-04 01:52:51 +0100

    Final update

--- src/components/FinalMessage.jsx
+++ src/components/FinalMessage.jsx
@@ -2,21 +2,25 @@ import React, { useEffect, useState } from 'react';
 import styles from './modules/FinalMessage.module.css';
 
 const FinalMessage = () => {
-  const message = "  Happy Birthday Gabreila!  ";
+  const message = "Haappy  Birthday,  Gabriela!";
   const [displayedMessage, setDisplayedMessage] = useState('');
 
   useEffect(() => {
     const letters = message.split('');
     let index = 0;
-    const interval = setInterval(() => {
-      if (index < letters.length - 1) {
-        setDisplayedMessage((prev) => prev + letters[index]);
-        index++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 200); 
+    const startTyping = () => {
+      const interval = setInterval(() => {
+        if (index < letters.length - 1) {
+          setDisplayedMessage((prev) => prev + letters[index]);
+          index++;
+        } else {
+          clearInterval(interval);
+        }
+      }, 200); 
+    };
 
+    const timeout = setTimeout(startTyping, 2000); 
+    
     return () => {
       clearInterval(timeout);
     };
@@ -24,11 +28,9 @@ const FinalMessage = () => {
 
   return (
     <div className={styles.messageContainer}>
-      {displayedMessage ? displayedMessage.split('').map((letter, index) => (
-        <span key={index} className={styles.letter}>
-          {letter}
-        </span>
-      )) : <span className={styles.placeholder}>Loading message...</span>}
+      {displayedMessage && displayedMessage.split('').map((letter, index) => (
+        <span key={index} className={styles.letter}>{letter}</span>
+      ))}
     </div>
   );
 };

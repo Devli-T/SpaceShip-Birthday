a5d76a67089ec1c6a1000c06c5642b11b20f612a
Devli-T <dthakkar0423@gmail.com>
2024-07-04 01:30:17 +0100

    finalmessagegabs

--- src/components/FinalMessage.jsx
+++ src/components/FinalMessage.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from 'react';
 import styles from './modules/FinalMessage.module.css';
 
 const FinalMessage = () => {
-  const message = "Happy Birthday!";
+  const message = "  Happy Birthday Gabreila!  ";
   const [displayedMessage, setDisplayedMessage] = useState('');
 
   useEffect(() => {
     const letters = message.split('');
     let index = 0;
     const interval = setInterval(() => {
-      if (index < letters.length) {
+      if (index < letters.length - 1) {
         setDisplayedMessage((prev) => prev + letters[index]);
         index++;
       } else {
@@ -22,11 +22,11 @@ const FinalMessage = () => {
 
   return (
     <div className={styles.messageContainer}>
-      {displayedMessage.split('').map((letter, index) => (
+      {displayedMessage ? displayedMessage.split('').map((letter, index) => (
         <span key={index} className={styles.letter}>
           {letter}
         </span>
-      ))}
+      )) : <span className={styles.placeholder}>Loading message...</span>}
     </div>
   );
 };

89013a56ec72e854fda543eab23b3689029f4708
AnUnsocialPigeon <jdturrell@Gmail.com>
2024-07-04 01:20:58 +0100

    Clown ver.

--- src/components/Space.tsx
+++ src/components/Space.tsx
@@ -8,11 +8,13 @@ import SpaceStation from '../models/SpaceStation';
 import AudioPlayer from './AudioPlayer';
 import Aestroid from '../models/Aestroid';
 import Skybox from '../models/Skybox';
+import FinalMessage from './FinalMessage';
 
 const Space = () => {
   const [isAesteroidClicked, setAesteroidClicked] = useState(false);
 
   const handleAesteroidClicked = () => {
+    console.log("Clicked!");
     if (isAesteroidClicked) return;
     setAesteroidClicked(true);
   };
@@ -23,7 +25,7 @@ const Space = () => {
         <Suspense fallback={null}>
           <ambientLight intensity={1.5}/>
           <OrbitControls minDistance={4} maxDistance={15} panSpeed={0}/>
-          <SpaceStation position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0.45, -0.5, 0]} />
+          <SpaceStation position={[0, 0.5, 0]} scale={[1, 1, 1]} rotation={[0.45, -0.5, 0]} />
           <Skybox size={1} position={[0, 0, 0]} scale={[1, 1, 1]}/>
           <Aestroid position={[0, 0, 5]} scale={[0.004, 0.004, 0.004]} onClick={handleAesteroidClicked}/>
           <Sparkles size={2} color={"#ffffff"} scale={[10,10,10]} />
@@ -32,6 +34,7 @@ const Space = () => {
       </Canvas>
       <div className="top-left-text">Loading resources from remote server may take time. Please be patient.</div>
       <div className="top-right-text">Search for the Asteroid!</div>
+      {isAesteroidClicked && <FinalMessage />}
       <AudioPlayer />
     </div>
   );

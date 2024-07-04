047950b6f9caf08f6281f689b4a6817c973d5438
AnUnsocialPigeon <jdturrell@Gmail.com>
2024-07-04 01:39:48 +0100

    Something?

--- src/components/FinalMessage.jsx
+++ src/components/FinalMessage.jsx
@@ -2,22 +2,29 @@ import React, { useEffect, useState } from 'react';
 import styles from './modules/FinalMessage.module.css';
 
 const FinalMessage = () => {
-  const message = "Happy Birthday!";
+  const message = "Haappy  Birthday, Gabriela!"; 
   const [displayedMessage, setDisplayedMessage] = useState('');
 
   useEffect(() => {
     const letters = message.split('');
     let index = 0;
-    const interval = setInterval(() => {
-      if (index < letters.length) {
-        setDisplayedMessage((prev) => prev + letters[index]);
-        index++;
-      } else {
-        clearInterval(interval);
-      }
-    }, 200); 
 
-    return () => clearInterval(interval);
+    const startTyping = () => {
+      const interval = setInterval(() => {
+        if (index < letters.length - 1) {
+          setDisplayedMessage((prev) => prev + letters[index]);
+          index++;
+        } else {
+          clearInterval(interval);
+        }
+      }, 200); 
+    };
+
+    const timeout = setTimeout(startTyping, 2000); 
+
+    return () => {
+      clearInterval(timeout);
+    };
   }, [message]);
 
   return (
